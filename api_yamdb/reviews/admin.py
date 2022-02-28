59ea3761d560700957e5ca00d6207f22754625a0
simatheone <samone07@mail.ru>
2022-02-28 22:23:57 +0300

    Зарегистрировал модели в админке

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from reviews.models import Categories, Genres, Titles
+from api_yamdb.settings import EMPTY_VALUE_ADMIN_PANEL
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    """Админ панель для модели Категории."""
+    list_display = ('pk', 'name', 'slug')
+    list_editable = ('name',)
+    list_filter = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    empty_value_display = EMPTY_VALUE_ADMIN_PANEL
+
+
+@admin.register(Genres)
+class GenresAdmin(admin.ModelAdmin):
+    """Админ панель для модели Жанры."""
+    list_display = ('pk', 'name', 'slug')
+    list_editable = ('name',)
+    list_filter = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    empty_value_display = EMPTY_VALUE_ADMIN_PANEL
+
+
+@admin.register(Titles)
+class TitlesAdmin(admin.ModelAdmin):
+    """Админ панель для модели Произведения."""
+    list_display = ('pk', 'name', 'year', 'description', 'category')
+    list_editable = ('name', 'year', 'description', 'category')
+    list_filter = ('name', 'year', 'category')
+    empty_value_display = EMPTY_VALUE_ADMIN_PANEL
