9430a475fb4d7b7152f9e7cb9a64236825352505
simatheone <samone07@mail.ru>
2022-02-28 23:03:59 +0300

    Добавил вьюсеты для моделей Категории, Жанры, Произведения

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,1 +1,25 @@
-from django.shortcuts import render
+# from rest_framework import filters
+from rest_framework import viewsets
+
+from api.serializers import (
+    CategoriesSerializer, GenresSerializer,
+    TitlesSerializer
+)
+from reviews.models import (
+    Categories, Genres, Titles
+)
+
+
+class CategoriesViewSet(viewsets.ModelViewSet):
+    queryset = Categories.objects.all()
+    serializer_class = CategoriesSerializer
+
+
+class GenresViewSet(viewsets.ModelViewSet):
+    queryset = Genres.objects.all()
+    serializer_class = GenresSerializer
+
+
+class TitlesViewSet(viewsets.ModelViewSet):
+    queryset = Titles.objects.all()
+    serializer_class = TitlesSerializer
