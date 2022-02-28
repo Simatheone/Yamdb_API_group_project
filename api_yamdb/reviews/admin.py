06584728a3d779c7b2569095f343b959b63f6223
ivi-x <stanislav@belenov.com>
2022-02-28 23:15:22 +0300

    add CustomUser admin.py

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "bio",
+        "role",
+        "is_staff",
+    )
+    search_fields = ("username",)
+    list_filter = ("role",)
+    empty_value_display = "-пусто-"
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,56 +2,47 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 
-USER_ROLE_USER = 'user'
-USER_ROLE_MODERATOR = 'moderator'
-USER_ROLE_ADMIN = 'admin'
+USER_ROLE_USER = "user"
+USER_ROLE_MODERATOR = "moderator"
+USER_ROLE_ADMIN = "admin"
 
 USER_ROLE_CHOICES = (
-    (USER_ROLE_USER, 'Пользователь'),
-    (USER_ROLE_MODERATOR, 'Модератор'),
-    (USER_ROLE_ADMIN, 'Админ'),
+    (USER_ROLE_USER, "Пользователь"),
+    (USER_ROLE_MODERATOR, "Модератор"),
+    (USER_ROLE_ADMIN, "Админ"),
 )
 
 
 class CustomUser(AbstractUser):
     username_validator = UnicodeUsernameValidator()
     username = models.CharField(
-        verbose_name='Пользователь',
+        verbose_name="Пользователь",
         max_length=257,
         unique=True,
-        help_text=(
-            'Представьтесь пожалуйста.'
-        ),
+        help_text=("Представьтесь пожалуйста."),
         validators=[username_validator],
         error_messages={
-            'unique': 'Пользователь с таким именем уже зарегистрирован',
+            "unique": "Пользователь с таким именем уже зарегистрирован",
         },
     )
     first_name = models.CharField(
-        verbose_name='Имя', max_length=257, blank=True
+        verbose_name="Имя", max_length=257, blank=True
     )
     last_name = models.CharField(
-        verbose_name='Фамилия', max_length=257, blank=True
+        verbose_name="Фамилия", max_length=257, blank=True
     )
     email = models.EmailField(
-        max_length=257,
-        unique=True,
-        verbose_name='Электронная почта'
+        max_length=257, unique=True, verbose_name="Электронная почта"
     )
     role = models.CharField(
         max_length=16,
         choices=USER_ROLE_CHOICES,
         default=USER_ROLE_USER,
-        verbose_name='Роль'
+        verbose_name="Роль",
     )
-    bio = models.TextField(
-        blank=True,
-        verbose_name='Биография'
-    )
+    bio = models.TextField(blank=True, verbose_name="Биография")
     confirmation_code = models.CharField(
-        max_length=50,
-        blank=True,
-        verbose_name='Код для авторизации'
+        max_length=50, blank=True, verbose_name="Код для авторизации"
     )
 
     class Meta:
@@ -59,8 +50,7 @@ class CustomUser(AbstractUser):
         verbose_name_plural = "Пользователи"
         constraints = [
             models.UniqueConstraint(
-                fields=['username', 'email'],
-                name='unique_username_email'
+                fields=["username", "email"], name="unique_username_email"
             )
         ]
 
@@ -68,6 +58,6 @@ class CustomUser(AbstractUser):
         return self.is_staff or self.role == USER_ROLE_ADMIN
 
     class Meta:
-        ordering = ('username',)
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
+        ordering = ("username",)
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
