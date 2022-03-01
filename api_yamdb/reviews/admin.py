32e353acb8eea14cea9a650834a7b5fa18c2ee5c
ivi-x <stanislav@belenov.com>
2022-03-01 20:11:32 +0300

    review Pycharm (2)

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,8 +1,7 @@
-from django.contrib import admin
 from django.conf import settings
+from django.contrib import admin
 
-from reviews.models import Categories, CustomUser, Genres, Titles
-from api_yamdb.settings import EMPTY_VALUE_ADMIN_PANEL
+from .models import Category, CustomUser, Genre, Title
 
 
 @admin.register(CustomUser)
@@ -21,30 +20,33 @@ class UserAdmin(admin.ModelAdmin):
     empty_value_display = settings.EMPTY_VALUE_ADMIN_PANEL
 
 
-@admin.register(Categories)
+@admin.register(Category)
 class CategoriesAdmin(admin.ModelAdmin):
     """Админ панель для модели Категории."""
-    list_display = ('pk', 'name', 'slug')
-    list_editable = ('name',)
-    list_filter = ('name', 'slug')
-    prepopulated_fields = {'slug': ('name',)}
-    empty_value_display = EMPTY_VALUE_ADMIN_PANEL
+
+    list_display = ("pk", "name", "slug")
+    list_editable = ("name",)
+    list_filter = ("name", "slug")
+    prepopulated_fields = {"slug": ("name",)}
+    empty_value_display = settings.EMPTY_VALUE_ADMIN_PANEL
 
 
-@admin.register(Genres)
+@admin.register(Genre)
 class GenresAdmin(admin.ModelAdmin):
     """Админ панель для модели Жанры."""
-    list_display = ('pk', 'name', 'slug')
-    list_editable = ('name',)
-    list_filter = ('name', 'slug')
-    prepopulated_fields = {'slug': ('name',)}
-    empty_value_display = EMPTY_VALUE_ADMIN_PANEL
+
+    list_display = ("pk", "name", "slug")
+    list_editable = ("name",)
+    list_filter = ("name", "slug")
+    prepopulated_fields = {"slug": ("name",)}
+    empty_value_display = settings.EMPTY_VALUE_ADMIN_PANEL
 
 
-@admin.register(Titles)
+@admin.register(Title)
 class TitlesAdmin(admin.ModelAdmin):
     """Админ панель для модели Произведения."""
-    list_display = ('pk', 'name', 'year', 'description', 'category')
-    list_editable = ('name', 'year', 'description', 'category')
-    list_filter = ('name', 'year', 'category')
-    empty_value_display = EMPTY_VALUE_ADMIN_PANEL
+
+    list_display = ("pk", "name", "year", "description", "category")
+    list_editable = ("name", "year", "description", "category")
+    list_filter = ("name", "year", "category")
+    empty_value_display = settings.EMPTY_VALUE_ADMIN_PANEL
