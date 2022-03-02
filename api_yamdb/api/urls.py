a5d5e9db23f90018ffb23944f27e942c2c6d98c2
simatheone <samone07@mail.ru>
2022-03-02 20:47:50 +0300

    Изменил названия импортируемых вьюсетов

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,13 +2,13 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api.views import (
-    CategoriesViewSet, GenresViewSet, TitlesViewSet
+    CategoryViewSet, GenreViewSet, TitleViewSet
 )
 
 router_v1 = DefaultRouter()
-router_v1.register(r'categories', CategoriesViewSet)
-router_v1.register(r'genres', GenresViewSet)
-router_v1.register(r'titles', TitlesViewSet)
+router_v1.register(r'categories', CategoryViewSet)
+router_v1.register(r'genres', GenreViewSet)
+router_v1.register(r'titles', TitleViewSet)
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
