599375a025ddce22a7e2cc933b4bcf0d23c1416e
simatheone <samone07@mail.ru>
2022-03-02 23:41:58 +0300

    Добавил фильтры для тайтлов и лукапы по слагу в жанрах и категориях

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import filters
 from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .pagination import CategoryPagination
 from .serializers import (
@@ -33,6 +34,7 @@ class CategoryViewSet(GetCreateDeleteViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields = ('=name',)
     pagination_class = CategoryPagination
+    lookup_field = 'slug'
 
     def update(self, request, *args, **kwargs):
         return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
@@ -48,6 +50,7 @@ class GenreViewSet(GetCreateDeleteViewSet):
     serializer_class = GenreSerializer
     permission_classes = None
     pagination_class = PageNumberPagination
+    lookup_field = 'slug'
 
 
 class TitleViewSet(viewsets.ModelViewSet):
@@ -59,6 +62,8 @@ class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
     permission_classes = None
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('category', 'genre', 'name', 'year')
     pagination_class = PageNumberPagination
 
     def update(self, request, *args, **kwargs):
