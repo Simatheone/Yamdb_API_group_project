58bcffb07f8131e484d9fea31beb0a6e66e2a8a3
simatheone <samone07@mail.ru>
2022-03-02 20:48:58 +0300

    Добавил базовый функционал во вьюсет

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,25 +1,65 @@
 # from rest_framework import filters
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import filters
+from rest_framework.pagination import PageNumberPagination
 
+from .pagination import CategoryPagination
 from .serializers import (
-    CategoriesSerializer, GenresSerializer,
-    TitlesSerializer
+    CategorySerializer, GenreSerializer,
+    TitleSerializer
 )
 from reviews.models import (
     Category, Genre, Title
 )
 
 
-class CategoriesViewSet(viewsets.ModelViewSet):
+class GetCreateDeleteViewSet(mixins.ListModelMixin, mixins.CreateModelMixin,
+                             mixins.DestroyModelMixin, viewsets.GenericViewSet
+                             ):
+    pass
+
+
+class CategoryViewSet(GetCreateDeleteViewSet):
+    """
+    Вьюсет для модели Category.
+    Обрабатывает запросы: GET, POST, DELETE
+    Эндпоинты: /categories/, /categories/{slug}/
+    """
     queryset = Category.objects.all()
-    serializer_class = CategoriesSerializer
+    serializer_class = CategorySerializer
+    permission_classes = None
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('=name',)
+    pagination_class = CategoryPagination
+
+    def update(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
-class GenresViewSet(viewsets.ModelViewSet):
+class GenreViewSet(GetCreateDeleteViewSet):
+    """
+    Вьюсет для модели Genre.
+    Обрабатывает запросы: GET, POST, DELETE
+    Эндпоинты: /genres/, /genres/{slug}/
+    """
     queryset = Genre.objects.all()
-    serializer_class = GenresSerializer
+    serializer_class = GenreSerializer
+    permission_classes = None
+    pagination_class = PageNumberPagination
 
 
-class TitlesViewSet(viewsets.ModelViewSet):
+class TitleViewSet(viewsets.ModelViewSet):
+    """
+    Вьюсет для модели Title.
+    Обрабатывает запросы: GET, POST, PATCH, DELETE, GET 1 элемента.
+    Эндпоинты: /titles/, /titles/{titles_id}/
+    """
     queryset = Title.objects.all()
-    serializer_class = TitlesSerializer
+    serializer_class = TitleSerializer
+    permission_classes = None
+    pagination_class = PageNumberPagination
+
+    def update(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
