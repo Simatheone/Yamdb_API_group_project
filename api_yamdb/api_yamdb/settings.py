837fd476e8209479b00badac010e601ae3dac466
ivi-x <stanislav@belenov.com>
2022-03-03 10:40:39 +0300

    Auth api 3

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -18,7 +18,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'rest_framework_simplejwt',
     'rest_framework',
-    'django_filters',
     "reviews.apps.ReviewsConfig",
     "api.apps.ApiConfig",
 ]
